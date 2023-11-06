d391c5e56d6af143b8a242743f31f3abd5e0be12
ulvio19 <007esedzade00@gmail.com>
2023-11-07 01:55:50 +0400

    home added v1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,17 +6,20 @@ const Header = () => {
     <header>
      <div className='header_boxes w-11/12 m-auto text-center py-3 flex justify-between'>
       <div className="header_logo">
-          <h1>Logo</h1>
+          <a>Logo</a>
       </div>
       <div className="navs">
           <ul className='flex justify-between'>
-               <li><a href="#">Home</a></li>
+               <li><a href="#" className='active'>Home</a></li>
                <li><a href="#">About</a></li>
                <li><a href="#">Education</a></li>
                <li><a href="#">Skills</a></li>
                <li><a href="#">Contact</a></li>
           </ul>
       </div>
+      <i class="bi bi-filter-right"></i>
+      
+
      </div>
     </header>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import './scss/home.scss'
+import img1 from "../images/about1.png"
 const Home = () => {
   return (
     <>
     <div className="home_sec1">
-      <div className="home_boxes w-11/12 m-auto md:flex">
+      <div className="home_boxes w-11/12 m-auto  md:flex gap-20">
       <div className="box1">
-        <h1>Hi,I'm Ulvi Esedzade</h1>
+        <h1>Hi,I'm <span>Ulvi Esedzade</span></h1>
+        <div className="text-animate">
         <h3>Frontend Developer</h3>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex ut recusandae magni molestiae, sequi obcaecati. At nisi deserunt nulla ducimus, molestiae sapiente aperiam animi illo voluptatibus ipsa assumenda quam exercitationem quaerat, quo vitae eaque vero quia similique asperiores totam nobis. Est qui vero, numquam culpa nulla tempore non tenetur molestias.
-        </p>
-        <div className="flex">
-          <button>click</button>
-          <button>click</button>
         </div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex ut recusandae magni molestiae, sequi obcaecati. At nisi deserunt nulla ducimus, molestiae sapiente aperiam animi illo voluptatibus .
+        </p>
+        <div className="btn-box">
+          <button className='btn'>click</button>
+          <button className='btn'>click</button>
+        </div>
+      </div>
+      <div className="box2 h-80">
+        <img src={img1} alt="" />
       </div>
       </div>
     </div>
